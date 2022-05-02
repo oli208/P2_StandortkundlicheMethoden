2a2ab45f505e5006fefc74cdb6e39c7075399d58
Oliver <68897677+oli208@users.noreply.github.com>
2022-05-02 13:26:34 +0200

    added Neubürg, some stylistic improvements

--- src/hangprofil.R
+++ src/hangprofil.R
@@ -14,43 +14,62 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 library(readxl)
 # PREPARE DATA #################################################################
 
-tbl_frankenhaag <- read_xlsx("Hangprofil.xlsx", sheet = 1, range = "A4:E22") #Einlesen der Daten 
-tbl_frankenhaag <- tbl_frankenhaag[,c(2,5)] # FIXME Aktualisierung der Ausgangstabelle
-
+tbl_frankenhaag <- read_xlsx("Hangprofil.xlsx", sheet = 1, range = "A4:B22") #Einlesen der Daten 
 tbl_neubuerg <- read_xlsx("Hangprofil.xlsx", sheet = 2, range = "A4:B12") #Einlesen der Daten 
 
-referenzhoehe <- 430
+attr(tbl_neubuerg, "referenzhoehe") <- 550
+attr(tbl_frankenhaag, "referenzhoehe") <- 430
+
 names(tbl_frankenhaag) <- c("Distanz", "Winkel") # Umbenennung der Tabellenüberschriften
+names(tbl_neubuerg) <- c("Distanz", "Winkel") # Umbenennung der Tabellenüberschriften
 
 head(tbl_frankenhaag)
 
-# AUSWEWRTUNG ##################################################################
-attach(tbl_frankenhaag)     # Speicherung der beiden Spalten im golbal env
-vc_length = Distanz[-1] - Distanz[-length(Distanz)] # Berechnung der Strecken zwischen den Abschnitten
+# FUNCTIONS  ###################################################################
 
-height <- vc_length * sin(Winkel*(pi/180)) #https://stackoverflow.com/questions/21402259/radian-measure-in-sin-in-r
-height = c(0,height[-length(height)])
-cbind(tbl_frankenhaag, height)
-
-sumheight = 0
-for(i in 2:length(height)){
-    sumheight[i] =  sum(sumheight[i-1], -height[i], na.rm = TRUE) 
+getheight <- function(distance, degree) {
+    vc_length = distance[-1] - distance[-length(distance)] # Berechnung der Strecken zwischen den Abschnitten
+    
+    height <- vc_length * sin(degree*(pi/180)) #https://stackoverflow.com/questions/21402259/radian-measure-in-sin-in-r
+    height = c(0,height[-length(height)])
+    
+    return(height)
 }
-sumheight_ges <- sumheight + referenzhoehe
+
+getgesheight <- function(height, refheight){
+    sumheight = refheight
+    for(i in 2:length(height)){
+        sumheight[i] =  sumheight[i-1] - height[i]
+    }
+    return(sumheight)
+}
+
+getplot <- function(distance, gesheight, refheight, title){
+    plot(gesheight~distance , type="o" , lwd=3, col=1 , 
+         ylim = c(min(gesheight)-10, refheight) , ylab="Höhe über NN (m)",
+         xlim = c(0, max(distance)+10),  xlab="Strecke (m)", 
+         main = title,  bty="l" , pch=20 , cex=1)
+    abline(h=seq(max(round(refheight, -1)), min(gesheight)*0.9,-10) , col="grey", lwd=0.8)
+}
+
+# AUSWERTUNG  ###################################################################
 
 
+nb_h <- getheight(tbl_neubuerg$Distanz, tbl_neubuerg$Winkel)
+nb_gh <- getgesheight(nb_h, attributes(tbl_neubuerg)$referenzhoehe)
+
+fh_h <- getheight(tbl_frankenhaag$Distanz, tbl_frankenhaag$Winkel)
+fh_gh <- getgesheight(fh_h, attributes(tbl_frankenhaag)$referenzhoehe)
 
 
 
 ## Plot ========================================================================
-plot(sumheight_ges~Distanz , type="o" , lwd=3, col=1 , 
-     ylim = c(min(sumheight_ges)-10, referenzhoehe) , ylab="Höhe über NN (m)",
-     xlim = c(0, max(Distanz)+10),  xlab="Strecke (m)", 
-     main = "Höhenprofil Frankenhaag",  bty="l" , pch=20 , cex=1)
-abline(h=seq(350,430,10) , col="grey", lwd=0.8)
+getplot(tbl_neubuerg$Distanz, nb_gh, attributes(tbl_neubuerg)$referenzhoehe, 
+        "Höhenprofil Neubürg")
 
+getplot(tbl_frankenhaag$Distanz, fh_gh, attributes(tbl_frankenhaag)$referenzhoehe, 
+        "Höhenprofil Frankenhaag")
 
-detach(tbl_frankenhaag)
 # CLEAN UP #####################################################################
 
 # clear Environment
@@ -67,6 +86,3 @@ dev.off() # But only if there is a plot
 
 # clear console
 cat("\014") # ctrl+L
-
-
-
